29c92fd48de26ccd12450a38fd981626148b6c94
amin modaresi <aminiiiiiiiuii@gmail.com>
2022-05-30 12:14:24 +0430

    fix rial to toman

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 let priceText = document.querySelector("#priceText");
 let persian = /^[\u0600-\u06FF\s]+$/;
-let RialToToman = (price) => price / 10;
+let RialToToman = (price) => price / 10 ;
 let form = document.querySelector("#pay");
 // input
 let Name = document.querySelector("input[name='name']");
@@ -89,28 +89,12 @@ payValue.addEventListener("blur", () => {
   payValue.classList.remove("border-danger");
 });
 payValue.addEventListener("input" , ()=>{
-  priceText.innerHTML = `${formatMoney(RialToToman(payValue.value) , 0 , "," , ".")} تومان`;
+  priceText.innerHTML = `${formatMoney(RialToToman(payValue.value))} تومان`;
 })
 
-function formatMoney(n, c, d, t) {
-  var c = isNaN(c = Math.abs(c)) ? 0 : c,
-    d = d == undefined ? "," : d,
-    t = t == undefined ? "." : t,
-    s = n < 0 ? "-" : "",
-    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
-    j = (j = i.length) > 3 ? j % 3 : 0;
-
-  return (
-    s +
-    (j ? i.substr(0, j) + t : "") +
-    i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) +
-    (c
-      ? d +
-        Math.abs(n - i)
-          .toFixed(c)
-          .slice(2)
-      : "")
-  );
+function formatMoney(money){
+  money = money.toString().includes(".") ? money.toString().slice(0, money.toString().indexOf(".")) : money.toString();
+  return money.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function addAlert(item , alert , alertText){
